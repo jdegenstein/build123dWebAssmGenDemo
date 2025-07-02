d90bac03678e866bc9f42900da9c89b8f882756a
jdegenstein <jdegenstein@users.noreply.github.com>
2025-07-02 08:31:16 -0500

    generate.py -> re-add lego compatible example

--- generate.py
+++ generate.py
@@ -1,21 +1,40 @@
 # PARAMETERS_START
 # {
-#   "length": {"type": "number", "default": 80.0, "min": 10, "max": 200, "step": 0.1, "label": "Length (mm)", "description": "Length of the box"},
-#   "width": {"type": "number", "default": 60.0, "min": 10, "max": 200, "step": 0.1, "label": "Width (mm)", "description": "Width of the box"},
-#   "thickness": {"type": "number", "default": 10.0, "min": 1, "max": 50, "step": 0.1, "label": "Thickness (mm)", "description": "Thickness of the box"},
-#   "center_hole_dia": {"type": "number", "default": 22.0, "min": 0, "max": 100, "step": 0.1, "label": "Center Hole Diameter (mm)", "description": "Diameter of the center hole"},
-#   "tubeSize": {"type": "number", "default": 18.0, "min": 0, "max": 100, "step": 0.1, "label": "Tube Size (mm)", "description": "Diameter of the larger half thickness hole"}
+#   "width": {"type": "number", "default": 4, "min": 1, "max": 200, "step": 1, "label": "Width (units)", "description": "Width of the block"},
+#   "length": {"type": "number", "default": 2, "min": 1, "max": 200, "step": 1, "label": "Length (units)", "description": "Length of the block"},
+#   "height": {"type": "number", "default": 4, "min": 1, "max": 50, "step": 1, "label": "Height (units)", "description": "Height of the block"},
 # }
 # PARAMETERS_END
+# TODO: look at max value for parameters
 
-with BuildPart() as boxHole:
-    Box(length, width, thickness)
-    Cylinder(radius=center_hole_dia / 2, height=thickness, mode=Mode.SUBTRACT)
-
-with BuildPart() as tube:
-    Cylinder(radius=tubeSize / 2, height=thickness * 4, rotation=(0, 0, 90))
+def lego_brick(pip_width: int = 2, pip_depth: int = 2, unit_block_height: int = 2):
+    pip_diameter = 4.8  # mm
+    pip_spacing = 8.0  # mm center-to-center
+    border_size = 7.8  # mm
+    block_width = (pip_width - 1) * pip_spacing + border_size  # mm
+    block_depth = (pip_depth - 1) * pip_spacing + border_size  # mm
+    block_height = unit_block_height * 3.2  # mm
+    pip_height = 1.6 + block_height  # mm
+    lego = Part() + extrude(Rectangle(block_width, block_depth), amount=block_height)
+    pips = Sketch() + GridLocations(
+        pip_spacing, pip_spacing, pip_width, pip_depth
+    ) * Circle(pip_diameter / 2)
+    lego += extrude(pips, amount=pip_height)
+    lego2 = offset(
+        lego,
+        amount=-1.2,
+        openings=lego.faces().sort_by(Axis.Z)[0],
+        kind=Kind.INTERSECTION,
+    )
+    if min((pip_width, pip_depth)) > 1:
+        mid_supp = Sketch() + GridLocations(
+            pip_spacing, pip_spacing, pip_width - 1, pip_depth - 1
+        ) * (Circle(6.5 / 2) - Circle(4.8 / 2))
+    elif min((pip_width, pip_depth)) == 1:
+        mid_supp = Sketch() + Circle(3 / 2) - Circle(3 / 2 - 0.8)
+    lego2 += extrude(mid_supp, amount=block_height)
+    return lego2
 
 output = [
-    {"name": "tube", "part": tube.part, "color": "#4f46e5", "opacity": 0.6},
-    {"name": "boxHole", "part": boxHole.part, "color": "#10b981"}
+    {"name": f"lego_brick_{width}x{length}x{height}", "part": lego_brick(width, length, height), "color": "#10b981"}
 ]
